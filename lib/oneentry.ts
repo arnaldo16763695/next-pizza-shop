1914cca60c6eab9e865924af6b4b98015da74ce2
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-08-21 17:53:45 -0400

    Add authentication with oneentry

--- lib/oneentry.ts
+++ lib/oneentry.ts
@@ -33,7 +33,7 @@ async function setupApiClient(): Promise<ReturnType<typeof defineOneEntry>> {
 
       // Create a new instance of the API client with the required configuration
       apiClient = defineOneEntry(apiUrl, {
-        token: process.env.ONENETRY_TOKEN, // Token for authentication
+        token: process.env.ONEENTRY_TOKEN, // Token for authentication
         langCode: 'en_US', // Language code for the API
         auth: {
           refreshToken: refreshToken || undefined, // Use the retrieved refresh token or `undefined`
@@ -90,4 +90,4 @@ export async function fetchApiClient(): Promise<
 
   // Return the initialized API client
   return apiClient;
-}
+} 
